7446a5bb99cdcb61540dabc6b2d9cf49e55bd97b
Patrekur M Magnusson <pmm1@hi.is>
2018-02-15 16:23:32 +0000

    added some packages to docker image

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential g++ python git
+apt-get install -y curl bzip2 build-essential g++ python git libcairo2-dev libjpeg-dev libpango1.0-dev libgif-dev
